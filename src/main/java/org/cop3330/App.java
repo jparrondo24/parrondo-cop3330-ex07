688e5dc80828f23cae7cf3a12b8dc24ae24edd00
Justin Parrondo <justinparrondo@gmail.com>
2021-06-06 01:18:44 -0400

    Initial commit

---
+++ src/main/java/org/cop3330/App.java
@@ -0,0 +1,24 @@
+package org.cop3330;
+
+import java.util.Scanner;
+
+public class App {
+    static final double CONVERSION_FACTOR = 0.09290304;
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("What is the length of the room in feet? ");
+        int length = in.nextInt();
+        System.out.print("What is the width of the room in feet? ");
+        int width = in.nextInt();
+        System.out.printf("You entered dimensions of %d feet by %d feet.\n", length, width);
+
+        int squareFeet = length * width;
+        double squareMeters = squareFeet * CONVERSION_FACTOR;
+
+        System.out.println("The area is");
+        System.out.printf("%d square feet\n", squareFeet);
+        System.out.printf("%.3f square meters\n", squareMeters);
+    }
+}
